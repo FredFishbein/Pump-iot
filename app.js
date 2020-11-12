88895399a20cdddb4485bef866a51b743e425fe3
2600 <a2600@Mac-AF.hsd1.ca.comcast.net>
2020-11-12 09:44:02 -0800

    Opacity change to navbar

--- app.js
+++ app.js
@@ -55,10 +55,10 @@ app.post("/", function (req, res) {
     };
 
     const jsonData = JSON.stringify(data);
-    const url = "ENTER MAILCHIMP URL/ API";
+    const url = "https://us2.api.mailchimp.com/3.0/lists/42e33c70ab";
     const options = {
         method: "POST",
-        auth: "ENTER USERNAME: API AUTH"
+        auth: "haha:51721a111b1f8f6bd50b49fde3e068ae-us2"
     }
     const request =https.request(url, options, function(response){
 
