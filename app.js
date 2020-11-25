0dafeb8af2b11a7d7d8514c13037c0de818c5dbf
2600 <a2600@Mac-AF.hsd1.ca.comcast.net>
2020-11-25 07:12:15 -0800

    new files

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require ("body-parser");
 const https = require("https");
 const request = require("request");
-
-    
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 
 const app = express();
@@ -12,36 +13,33 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set('view engine','ejs');
 
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser:true});
+const userSchema = new mongoose.Schema ({
+    email:String,
+    password:String
+});
 
+userSchema.plugin(encrypt,{ secret: process.env.SECRET, encryptedFields: ['password']});
+const User = new mongoose.model("User", userSchema);
 
+// var today = new Date();
 
+//     var options = {
+//         weekday:"short",
+//         day:"numeric",
+//         month:"long",
 
+//     };
 
-
-var today = new Date();
-
-    var options = {
-        weekday:"short",
-        day:"numeric",
-        month:"long",
-
-    };
-
-    var day = today.toLocaleDateString("en-US", options);
+// var day = today.toLocaleDateString("en-US", options);
     
 
-    
-
-    
 
 app.get ("/", function(req, res){
-    res.sendFile(__dirname + "/list.ejs");
-    res.render("list", {kindOfDay:day});
+    res.render("Main");
 })
 
-
-
-
+// Signup/Contact Section
 app.post("/contact", function (req, res) {
     const firstName = req.body.fName;
     const lastName = req.body.lName;
@@ -66,9 +64,7 @@ app.post("/contact", function (req, res) {
                     LNAME:lastName,
                     MESSAGE:messageBody,
                     TITLE:title,
-                    CITY:cityName
-
-                    
+                    CITY:cityName   
                 }
                 
             }
@@ -76,17 +72,17 @@ app.post("/contact", function (req, res) {
     };
 
     const jsonData = JSON.stringify(data);
-    const url = "https://us2.api.mailchimp.com/3.0/lists/42e33c70ab";
+    const url= "process.env.URL";
     const options = {
         method: "POST",
-        auth: "haha:51721a111b1f8f6bd50b49fde3e068ae-us2"
+        auth: "process.env.AUTH"
     }
     const request =https.request(url, options, function(response){
 
         if(response.statusCode === 200){
-            res.sendFile(__dirname + "/success.html");
+            res.render("Success");
         }else{
-            res.sendFile(__dirname +"/failure.html")
+            res.render("Failure")
         }
 
         response.on("data", function(data){
@@ -107,17 +103,56 @@ app.post("/success", function(req, res){
 });
 app.get("/blog", function(req,res){
     res.render("blog");
-   });
+});
 app.get("/about", function(req,res){
     res.render("about");
-   });
+});
    app.get("/contact", function(req,res){
     res.render("contact");
-   });   
+}); 
+app.get("/Success", function(req,res){
+    res.render("Success");
+});  
+app.get("/Failure", function(req,res){
+    res.render("Failure");
+}); 
+app.get("/login", function(req,res){
+    res.render("login");
+});
+app.get("/register", function(req,res){
+    res.render("register");
+});
 
-  
 
+app.post("/register", function (req,res) {
+    const NewUser = new User ({
+        email: req.body.username,
+        password:req.body.password
+    });
+    NewUser.save(function(err){
+        if (err){
+            console.log(err)
+        }else{
+            res.render("login");
+        }    
+    })
+});
+app.post("/login", function(req,res) {
+    const username = req.body.username;
+    const password =req.body.password;
 
+    User.findOne({email:username}, function(err, foundUser){
+    if(err) {
+        console.log(err);
+        }else{
+            if(foundUser) {
+                if(foundUser.password === password){
+                    res.render("test");
+                }
+            }
+        }
+    });
+});
 
 
 
